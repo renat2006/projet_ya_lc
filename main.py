93c010d3ed290eb23bcf8ece2e9ea34ed21095ab
Renat Gubaydullin <renatgubaudullin@gmail.com>
2021-10-29 15:02:35 +0300

    Second task

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sys
-import time
-
+# import time
+# import comtypes.client
+# import win32com
+from PyQt5.QtGui import QPixmap
+# from pdf2image import convert_from_path
 from PyQt5 import uic
 from PyQt5.QtCore import QPropertyAnimation, QPoint, QSize, QParallelAnimationGroup, QTimer
 from PyQt5.QtWidgets import *
@@ -19,13 +22,6 @@ class Main_window(QWidget):
         self.w2.show()
 
 
-class Window3(QWidget):
-
-    def __init__(self):
-        super(Window3, self).__init__()
-        uic.loadUi('total.ui', self)
-
-
 class Window2(QWidget):
 
     def __init__(self):
@@ -41,6 +37,7 @@ class Window2(QWidget):
         self.left_btn.clicked.connect(self.resize_anim2)
 
     def show_window_3(self):
+
         self.close()
         self.w3 = Window3()
         self.w3.show()
@@ -96,6 +93,42 @@ class Window2(QWidget):
     def on_close(self):
         self.left_btn.setDisabled(False)
         self.right_btn.setDisabled(False)
+
+    def get_id(self):
+
+        self.objs = QFrame.findChildren(self.frame, QLabel)
+        self.biggest = self.objs[0]
+        for i in self.objs:
+            if i.width() > self.biggest.width():
+                self.biggest = i
+        return self.biggest.objectName()
+
+
+class Window3(QWidget):
+    # def PPTtoPDF(self, inputFileName, outputFileName, formatType=32):
+    # powerpoint = comtypes.client.CreateObject("Powerpoint.Application")
+    # powerpoint.Visible = 1
+
+    # if outputFileName[-3:] != 'pdf':
+    # outputFileName = outputFileName + ".pdf"
+    # deck = powerpoint.Presentations.Open(inputFileName)
+    # deck.SaveAs(outputFileName, formatType)
+    # deck.Close()
+    # powerpoint.Quit()
+
+    def __init__(self):
+        print(Window2().get_id())
+        super(Window3, self).__init__()
+        uic.loadUi('total.ui', self)
+        pixmap = QPixmap(f'3x/{Window2().get_id()}.png')
+        self.label_2.setPixmap(pixmap)
+        print(f'templates/{Window2().get_id()}.pptx')
+        # self.PPTtoPDF(f'{Window2().get_id()}.pptx', f'{Window2().get_id()}')
+
+
+class Con_checker():
+    def check(self):
+        print('OK')
 
 
 def except_hook(cls, exception, traceback):
---
+++ template_chooser.py
@@ -0,0 +1,12 @@
+import sys
+import time
+
+from PyQt5 import uic
+from PyQt5.QtCore import QPropertyAnimation, QPoint, QSize, QParallelAnimationGroup, QTimer
+from PyQt5.QtWidgets import *
+from main import *
+
+
+class Tem_chooser():
+    def get_id(self):
+        pass
